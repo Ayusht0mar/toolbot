17789073a8ed6348560c976805495e02913ec7bd
Ayush tomar <ayushtomar388@gmail.com>
2025-01-13 23:19:34 +0530

    Hydration surpress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
           <body
             className={` ${font.className} antialiased`}
           >
